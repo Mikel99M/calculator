9760015ad2bd72d422b3881b2cad1711f6433292
Michał Mielcarek <mike249mielcarek@gmail.com>
2024-10-26 15:26:34 +0200

    First commit

---
+++ src/main/java/com/kodilla/calculator/Calculator.java
@@ -0,0 +1,15 @@
+package com.kodilla.calculator;
+
+class Kalkulator {
+
+    public void someMethod() {
+        System.out.println("It works!");
+    }
+}
+
+public class Calculator {
+    public static void main(String[] args) {
+        Kalkulator calculator = new Kalkulator();
+        calculator.someMethod();
+    }
+}
